37567f79fb098c1073757b283f19207f2557b432
tommygranstrom <tommy.granstrom@hotmail.se>
2021-08-04 21:38:08 +0200

    Hej

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+//import { StyleSheet, Text, View, Button} from 'react-native';
+import StartScreen from './modules/startScreen';
+
+const Stack = createStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<NavigationContainer>
+             <Stack.Navigator>
+             <Stack.Screen
+                name="Home"
+                component={StartScreen}
+                options={{ title: 'Welcome' }}
+              />
+              <Stack.Screen name="Profile" component={ProfileScreen} />
+          </Stack.Navigator>
+          </NavigationContainer>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
